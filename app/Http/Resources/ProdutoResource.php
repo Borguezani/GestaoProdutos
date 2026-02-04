855b568d73dcc67ddcd1257af8221450c8e096be
Daniel Borguezani <daniel_borguezani@hotmail.com>
2026-02-04 10:32:38 -0300

    feat(Resources): Adiciona CategoriaResource e ProdutoResource para gerenciamento de categorias e produtos

---
+++ app/Http/Resources/ProdutoResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProdutoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'valor' => (float) $this->valor,
+            'quantidade' => $this->quantidade,
+            'categoria' => new CategoriaResource($this->whenLoaded('categoria')),
+            'usuario' => [
+                'id' => $this->usuario->id,
+                'nome' => $this->usuario->name,
+            ],
+        ];
+    }
+}
