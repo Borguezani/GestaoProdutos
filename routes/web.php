711b8a1a5257eef84101ed2e6ac0f38fceb084f6
Daniel Borguezani <daniel_borguezani@hotmail.com>
2026-02-04 10:31:43 -0300

    feat(Routes): Atualiza rotas para redirecionar para login e organiza recursos de produtos e categorias

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
@@ -12,9 +14,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Produtos
+    Route::resource('produtos', ProdutoController::class);
+    
+    // Categorias
+    Route::resource('categorias', CategoriaController::class)->except(['show']);
 });
 
 require __DIR__.'/auth.php';
